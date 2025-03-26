4938a019f5a08ad6144ba6f0b8362709b07ece5d
Mohana Krishnan <mohankk40@gmail.com>
2025-03-26 19:31:44 +0530

    binary serch update

---
+++ Algo_Names/binary_search.py
@@ -0,0 +1,16 @@
+
+def binary_search(names, target):
+  first = 0
+  last = len(names) - 1
+  while  first <= last:
+    mid = (first + last) // 2
+    if names[mid] == target:
+      return mid
+    elif names[mid] < target:
+      first = mid + 1
+    else:
+      last = mid - 1
+
+names = ["Anjali", "Arjun", "Karthik", "Mohana", "Priya", "Ravi"]
+
+print("Index: %d" % binary_search(names, "Arjun"))
